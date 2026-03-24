98979a2ab50c843751bcda8ad9dc42ff715b8ee7
tanazd1-octo <tanazd1@gmail.com>
2026-03-24 23:23:52 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/env bash
+
+echo "[*] Installing termux-miscallious..."
+
+BIN_DIR="$PREFIX/bin"
+
+for cmd in *; do
+    # skip non-files and installer files
+    [ -f "$cmd" ] || continue
+    case "$cmd" in
+        install.sh|uninstall.sh|README.md) continue ;;
+    esac
+
+    # make executable
+    chmod +x "$cmd"
+
+    # install to $PREFIX/bin
+    install -Dm755 "$cmd" "$BIN_DIR/$cmd"
+
+    echo "[✓] Installed $cmd"
+done
+
+echo "[✓] Done!"
